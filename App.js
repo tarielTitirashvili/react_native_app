068361b4274dfc4823dc79cde9ec6847b6a6196e
tarielTitirashvili <tarieltitirashvili@gmail.com>
2025-06-22 01:17:19 +0400

    first good looking layout

--- App.js
+++ App.js
@@ -7,32 +7,50 @@ export default function App() {
   return (
     <View style={styles.appContainer} >
       <View style={styles.inputContainer}>
-        <TextInput style={styles.inputStyles} placeholder='To Do' onChangeText={handleChangeText} value={value} />
+        <TextInput placeholderTextColor='gray' style={styles.inputStyles} placeholder='To Do' onChangeText={handleChangeText} value={value} />
         <Button color="purple" title='Add To Do' />
       </View>
-      <View></View>
+      <View style={styles.goalsContainer}>
+        <Text style={styles.listsTitleContainer}>
+          List Of To Does
+        </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    backgroundColor: '#fff',
+    flex: 1,
+    backgroundColor: 'black',
     padding: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
   inputContainer:{
+    flex: 1,
     paddingTop: 24,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 8
+    gap: 8,
+    paddingBottom: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: 'purple'
   },
   inputStyles:{
+    color: 'skyblue',
     padding: 8,
     borderWidth: 1,
-    borderBlockColor: 'purple',
+    borderColor: 'skyblue',
     width: '70%',
-    borderRadius: 6
+    borderRadius: 6,
+  },
+  goalsContainer:{
+    padding: 8,
+    paddingHorizontal: 0,
+    flex: 11
+  },
+  listsTitleContainer:{
+    color: 'skyblue'
   }
 })
