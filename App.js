f8ecef4f5efcd4ffd7fdeb02d56462e8d6890bc0
tarielTitirashvili <tarieltitirashvili@gmail.com>
2025-06-21 23:59:30 +0400

    testing styles

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button style={styles.button} title='tariel 1' />
+      <Text style={styles.text}>Hello World!! from Tariel!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    gap: 12,
+    backgroundColor: 'black',
+    color: 'purple !important',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    borderWidth: 3,
+    borderColor: '#fff',
+    color: 'purple',
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
